cc514a59e25cd1946584e3d5c23ea127c011747e
jbirky <jbirky@uw.edu>
2021-02-28 20:11:17 -0800

    initial commit

---
+++ utils.py
@@ -0,0 +1,77 @@
+import numpy as np 
+import vplot as vpl 
+import os
+import re
+import subprocess
+import time
+
+__all__ = ["VplanetModel"]
+
+
+class VplanetModel(object):
+
+    def __init__(self, params, **kwargs):
+        """
+        param   : (str, list) variable parameter names
+                  ['vpl.dStopTime', 'star.dRotPeriod', 'star.dMass', 'planet.dEcc', 'planet.dOrbPeriod']
+                
+        inpath  : (str) path to template infiles
+                  'infiles/'
+
+        outpath : (str) path to where model infiles should be written
+                  'output/'
+        """
+
+        self.params = params
+        self.inpath = kwargs.get('inpath', '.')
+        self.outpath = kwargs.get('outpath', '.')
+        self.vplfile = kwargs.get('vplfile', 'vpl.in')
+        self.sys_name = kwargs.get('sys_name', 'system')
+        
+        self.infile_list = kwargs.get('infile_list', os.listdir(self.inpath))
+
+
+    def initialize_model(self, theta):
+        """
+        theta   : (float, list) parameter values, corresponding to self.param
+        """
+        
+        theta = np.array(theta)
+        
+        param_file_all = np.array([x.split('.')[0] for x in params])  # e.g. ['vpl', 'primary', 'primary', 'secondary', 'secondary']
+        param_name_all = np.array([x.split('.')[1] for x in params])  # e.g. ['dStopTime', 'dRotPeriod', 'dMass', 'dRotPeriod', 'dMass']
+
+        for file in self.infile_list: # vpl.in, primary.in, secondary.in
+            with open(os.path.join(self.inpath, file), 'r') as f:
+                file_in = f.read()
+                
+            ind = np.where(param_file_all == file.strip('.in'))[0]
+            theta_file = theta[ind]
+            param_name_file = param_name_all[ind]
+            
+            if file == 'vpl.in':
+                file_in = re.sub("%s(.*?)#" % "sSystemName", "%s %s #" % ("sSystemName", self.sys_name), file_in)
+                
+            for i in range(len(theta_file)):
+                file_in = re.sub("%s(.*?)#" % param_name_file[i], "%s %.6e #" % (param_name_file[i], theta_file[i]), file_in)
+
+            with open(os.path.join(self.outpath, file), 'w') as f:
+                print(file_in, file = f)
+
+
+    def run_model(self, theta, remove=False, verbose=True):
+        """
+        theta   : (float, list) parameter values, corresponding to self.param
+        remove  : (bool) True will erase input/output files after model is run
+        """
+
+        self.initialize_model(theta)
+        
+        t0 = time.time()
+        subprocess.call(["vplanet vpl.in"], cwd=self.outpath, shell=True)
+        output = vpl.GetOutput(self.outpath, logfile=self.sys_name+'.log')
+        
+        if verbose == True:
+            print('Executed model %svpl.in %.3f s'%(self.outpath, time.time() - t0))
+            
+        return output
