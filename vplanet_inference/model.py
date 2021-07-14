78a5c487aa6e45f2316104a3f71d543d4378512c
Jessica Birky <jbirky@uw.edu>
2021-07-14 11:25:50 -0700

    get rid of functions

--- vplanet_inference/model.py
+++ vplanet_inference/model.py
@@ -32,7 +32,7 @@ class VplanetModel(object):
         self.infile_list = kwargs.get('infile_list', os.listdir(self.inpath))
 
 
-    def InitializeModel(self, theta, **kwargs):
+    def initialize_model(self, theta, **kwargs):
         """
         theta   : (float, list) parameter values, corresponding to self.param
 
@@ -70,7 +70,7 @@ class VplanetModel(object):
                 print(file_in, file = f)
 
 
-    def GetOutparam(self, output, outparams, **kwargs):
+    def get_outparam(self, output, outparams, **kwargs):
         """
         output    : (vplot object) results form a model run obtained using vplot.GetOutput()
 
@@ -89,7 +89,7 @@ class VplanetModel(object):
         return outvalues
 
 
-    def RunModel(self, theta, remove=False, verbose=True, **kwargs):
+    def run_model(self, theta, remove=False, verbose=True, **kwargs):
         """
         theta     : (float, list) parameter values, corresponding to self.params
 
@@ -99,7 +99,7 @@ class VplanetModel(object):
                     ['initial.primary.Luminosity', 'final.primary.Radius', 'initial.secondary.Luminosity', 'final.secondary.Radius']
         """
 
-        self.InitializeModel(theta, **kwargs)
+        self.initialize_model(theta, **kwargs)
         
         t0 = time.time()
         subprocess.call(["vplanet vpl.in"], cwd=self.outpath, shell=True)
@@ -112,33 +112,14 @@ class VplanetModel(object):
 
         if 'outparams' in kwargs:
             self.outparams = kwargs['outparams']  
-            outvalues = self.GetOutparam(output, self.outparams)
+            outvalues = self.get_outparam(output, self.outparams)
             return outvalues
 
         else:
             return output
 
 
-    def RunModelBatch(self, theta_list, remove=False, verbose=True, **kwargs):
-
-        # run models in parallel
-        if 'ncore' in kwargs:
-            ncore = kwargs['ncore']
-
-            """
-            to be implemented
-            """
-
-        # run models sequentially
-        else:  
-            for i, tt in enumerate(theta_list):
-                op = os.path.join(outpath, 'model%s/'%(i))
-                vpm.RunModel(tt, outpath=op)
-
-        return None
-
-
-    def InitializeBayes(self, data=None, outparams=None, bounds=None):
+    def initialize_bayes(self, data=None, outparams=None, bounds=None):
         """
         data      : (float, matrix)
                     [(rad, radSig), 
@@ -167,108 +148,14 @@ class VplanetModel(object):
             self.bounds = np.empty(shape=(self.nparams, 2), dtype='object') 
 
     
-    def LnLike(self, theta):
+    def lnlike(self, theta):
         """
         Gaussian likelihood function comparing vplanet model and given observational values/uncertainties
         """
 
-        ymodel = self.RunModel(theta, outparams=self.outparams)
+        ymodel = self.run_model(theta, outparams=self.outparams)
 
         # Gaussian likelihood 
         lnlike = -0.5 * np.sum(((ymodel - self.data.T[0])/self.data.T[1])**2)
 
         return lnlike
-
-
-    def LnPriorFlat(self, theta):
-        """
-        theta   : (float, list) parameter values, corresponding to self.param
-
-        bounds  : (float, matrix) min and max values for parameter in theta
-                  [(min_theta_0, max_theta_0),
-                   ...
-                   (min_theta_n, max_theta_n)]
-        """
-
-        theta = np.array(theta)
-
-        if theta.shape[0] != self.bounds.shape[0]:
-            print('Error: dim theta != dim bounds')
-            raise
-
-        for i in range(nparam):
-            min_bound, max_bound = self.bounds[i]
-
-            if min_bound != None:
-                if theta < min_bound:
-                    return -np.inf 
-            
-            if max_bound != None:
-                if theta > max_bound:
-                    return -np.inf 
-
-            # if (theta[i] < self.bounds[i][0]) or (theta[i] > self.bounds[i][1]):
-            #     return -np.inf
-
-        return 0
-
-
-    def LnPriorSample(self, size=1, LnPrior=self.LnPriorFlat, **kwargs):
-        """
-        prior format for approxposterior; defaults to uniform prior unless otherwise specified
-
-        bounds  : (float, matrix) min and max values for parameter in theta
-                  [(min_theta_0, max_theta_0),
-                   ...
-                   (min_theta_n, max_theta_n)] 
-
-        prior_dist : (list) specify custom non-uniform prior
-                     [np.random.uniform(low=0.07, high=0.11),
-                     norm.rvs(loc=fsatTrappist1, scale=fsatTrappist1Sig, size=1)[0],
-                     np.random.uniform(low=0.1, high=12),
-                     norm.rvs(loc=ageTrappist1, scale=ageTrappist1Sig, size=1)[0],
-                     norm.rvs(loc=betaTrappist1, scale=betaTrappist1Sig, size=1)[0]]
-        """
-
-        uniform_sample = np.random.uniform(self.bounds.T[0], self.bounds.T[1])
-
-        ret = []
-        for ii in range(size):
-            while True:
-                guess = kwargs.get('prior_dist', uniform_sample)
-                if not np.isinf(LnPrior(guess, **kwargs)):
-                    ret.append(guess)
-                    break
-        if size > 1:
-            return np.array(ret)
-        else:
-            return np.array(ret[0])
-
-
-    def PriorTransform(self, theta):
-        """
-        prior format for nested sampling
-
-        theta   : (float, list) parameter values, corresponding to self.param
-        """
-
-        pt = np.zeros(len(self.bounds))
-        for i, b in enumerate(self.bounds):
-            pt[i] = (b[1] - b[0]) * theta[i] + b[0]
-
-        return pt
-
-
-    def LnPosterior(self, theta, LnLike=self.LnLike, LnPrior=self.LnPriorFlat):
-        """
-        Compute LnPosterior = LnLiklihood + LnPrior
-
-        theta   : (float, list) parameter values, corresponding to self.param
-        """
-
-        return LnLike(theta) + LnPrior(theta)
-
-
-    def PosteriorSweep(self, theta_list, LnLike=self.LnLike, LnPrior=self.LnPriorFlat):
-
-        return None
