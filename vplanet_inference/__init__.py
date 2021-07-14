1ea663dfe988d132fb21d836c9d533e3de5e0eb7
Jessica Birky <jbirky@uw.edu>
2021-07-14 11:25:27 -0700

    get package path

--- vplanet_inference/__init__.py
+++ vplanet_inference/__init__.py
@@ -1,2 +1,8 @@
 from .model import *
-from .analysis import *
+
+import os
+
+# Get directory of vplanet_inference 
+PATH 	   = os.path.realpath(__file__)
+VPI_DIR    = os.path.split(os.path.split(PATH)[0])[0]
+INFILE_DIR = os.path.join(VPI_DIR, "infile")
